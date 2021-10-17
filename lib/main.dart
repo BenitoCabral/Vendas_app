f59e0f95d1bf46ba35250147a02c0b38c9d66111
BenitoCabral <benitocabralneres@gmail.com>
2021-10-17 10:15:29 -0300

    inicia firebase em main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:vendas_app/pages/login.page.dart';
+import 'package:vendas_app/pages/authentication.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyApp',
+      title: 'Vendas App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
-      home: LoginPage(),
-      //home: MyHomePage(title: 'Flutter Demo Teste Daniel'),
+      home: Authentication(),
     );
   }
 }
